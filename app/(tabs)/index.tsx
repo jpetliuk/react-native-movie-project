b09ad3ca968ed62e64e2bdd420fe0be8981f5c24
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-05-09 02:58:11 -0300

    feat: add movie fetching functionality and implement useFetch hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,26 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import {
+   ActivityIndicator,
+   FlatList,
+   Image,
+   ScrollView,
+   Text,
+   View,
+} from "react-native";
 
 export default function Index() {
    const router = useRouter();
+
+   const {
+      data: movies,
+      loading: moviesLoading,
+      error: moviesError,
+   } = useFetch(() => fetchMovies({ query: "" }));
 
    return (
       <View className="flex-1 bg-primary">
@@ -21,12 +36,34 @@ export default function Index() {
                className="w-12 h-10 mt-20 mb-5 mx-auto"
             />
 
-            <View className="flex-1 mt-5">
-               <SearchBar
-                  onPress={() => router.push("/search")}
-                  placeholder="Search for a Movie"
+            {moviesLoading ? (
+               <ActivityIndicator
+                  size="large"
+                  color="#0000ff"
+                  className="mt-10 self-center"
                />
-            </View>
+            ) : moviesError ? (
+               <Text>Error: {moviesError?.message}</Text>
+            ) : (
+               <View className="flex-1 mt-5">
+                  <SearchBar
+                     onPress={() => router.push("/search")}
+                     placeholder="Search for a Movie"
+                  />
+
+                  <Text className="text-lg text-white font-bold mt-5 mb-3">
+                     Latest Movies
+                  </Text>
+
+                  <FlatList
+                     data={movies}
+                     renderItem={({ item }) => (
+                        <Text className="text-white text-sm">{item.title}</Text>
+                     )}
+                     keyExtractor={}
+                  />
+               </View>
+            )}
          </ScrollView>
       </View>
    );
