04ad5837955e9a21937a8f3ea8f2f27954afb86a
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-05-09 22:54:51 -0300

    feat: add MovieDetails component and enhance Appwrite service for search count tracking

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from "react-native-appwrite";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
@@ -10,14 +10,53 @@ const client = new Client()
 const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
-   const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.equal("searchTerm", query),
-   ]);
+   try {
+      const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+         Query.equal("searchTerm", query),
+      ]);
 
-   console.log(result);
+      if (result.documents.length > 0) {
+         const existingMovie = result.documents[0];
+         await database.updateDocument(
+            DATABASE_ID,
+            COLLECTION_ID,
+            existingMovie.$id,
+            {
+               count: existingMovie.count + 1,
+            }
+         );
+      } else {
+         await database.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID,
+            ID.unique(),
+            {
+               searchTerm: query,
+               movie_id: movie.id,
+               title: movie.title,
+               count: 1,
+               poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            }
+         );
+      }
+   } catch (error) {
+      console.error("Error updating search count:", error);
+      throw error;
+   }
+};
 
-   // check if a record of that search has already been stored
-   // if a document is found increment the searchCount field
-   // if no document is found
-   // create a new document in Appwrite database
+export const getTrendingMovies = async (): Promise<
+   TrendingMovie[] | undefined
+> => {
+   try {
+      const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+         Query.limit(5),
+         Query.orderDesc("count"),
+      ]);
+
+      return result.documents as unknown as TrendingMovie[];
+   } catch (error) {
+      console.error(error);
+      return undefined;
+   }
 };
